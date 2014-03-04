b040c274945fb89baf569c1c08901e55522ac72d
Ashley Deane <bbbiiittt1198@gmail.com>
2014-03-04 00:24:01 +0000

    Cleaned up ServerSocV2 + Add Comments
    Cleaned up code and added more descriptive comments to help explain each part.

--- Vote Server-Java/ServerSocV2.java
+++ Vote Server-Java/ServerSocV2.java
@@ -9,9 +9,9 @@ class Lists
 	public static String [] songs = new String[100]; //Directory location of songs
 	public static String [] votes = new String[5]; //Current Vote Options
 	public static int voteCast = 0; //Number of votes cast
-	public static int clientNum = 0;
-	public static int firstcount=0;
-	public static int [] choice = new int [5];
+	public static int clientNum = 0; //Number of Active Clients
+	public static boolean firstSong = true; //Boolean to check if first song 
+	public static int [] choice = new int [5]; //holds the vote results
 }
 
 //Main Class----------------------------------------------------------------------------
@@ -57,7 +57,7 @@ class ServerSocV2
 				Lists.votes[i] = Lists.tracks[RnG(count)];
 			}
 
-
+			//Start Lisnting for connections
 			System.out.println("Listening for connections");
 			boolean listening= true;
 			ServerSocket serverSocket= new ServerSocket(7000);
@@ -85,7 +85,8 @@ class ServerSocV2
 class ClientHandler extends Thread
 {
 	private Socket socket = null;
-
+	
+	//RnG for filling vote options
 	public static int RnG(int i)
 	{
 		Random rn = new Random();
@@ -109,92 +110,108 @@ class ClientHandler extends Thread
 			while(k == 0)
 			{
 			//Output Song for Vote
-			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-			for(int i=0;i<5;i++)
-			{
-	           	out.writeUTF(Lists.votes[i]);
-	           	System.out.println(Lists.votes[i]);
-          	}
-
-		  	//Get vote option
-			DataInputStream in = new DataInputStream(socket.getInputStream());
-			String song=""+in.readUTF();
-			
-			
-			for(int i=0;i<5;i++)
-			{
-				if(song.equals(Lists.votes[i]))
-					Lists.choice[i]++;
-			}
-
-			Lists.voteCast = Lists.voteCast + 1; //Inc VoteCast
-			System.out.println(Lists.voteCast);
-			System.out.println(Lists.clientNum);
-
-
-			System.out.println(song); //Print Vote
-			while(Lists.voteCast != Lists.clientNum)
-			{}
-			try
-			{
-
-			//////waiting time for voting between songs
-			if(Lists.firstcount==0)
-			{
-				int finalchoice = 0;
-				for(int i = 1; i<5; i++)
+				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+				for(int i=0;i<5;i++)
 				{
-					if(Lists.choice[i]>Lists.choice[finalchoice])
-						finalchoice=i;
-				}
-			
-
-				for(int i=0;i<Lists.tracks.length;i++)
-				{		
-					if(Lists.votes[finalchoice].equals(Lists.tracks[i]))
-					{
-						song = Lists.songs[i];
-					}
-				}
-			Process proc = Runtime.getRuntime().exec("C:\\Program Files (x86)\\foobar2000\\foobar2000.exe  C:\\Users\\Luke\\Desktop\\music\\"+song);
-			Lists.firstcount++;
-			}
-			else
-			{
-				int finalchoice = 0;
-				for(int i = 1; i<5; i++)
+		           		out.writeUTF(Lists.votes[i]);
+		           		System.out.println(Lists.votes[i]); //Print songs server-side
+	          		}
+	
+			  	//Get vote option
+				DataInputStream in = new DataInputStream(socket.getInputStream());
+				String song=""+in.readUTF();
+				
+				//Assigning vote to correct song
+				for(int i=0;i<5;i++)
 				{
-					if(Lists.choice[i]>Lists.choice[finalchoice])
-						finalchoice=i;
+					if(song.equals(Lists.votes[i]))
+						Lists.choice[i]++;
 				}
-				for(int i=0;i<Lists.tracks.length;i++)
-				{		
-					if(Lists.votes[finalchoice].equals(Lists.tracks[i]))
+	
+				Lists.voteCast = Lists.voteCast + 1; //Inc VoteCast
+				
+				//Print updated stats
+				System.out.println(Lists.voteCast);
+				System.out.println(Lists.clientNum);
+
+				System.out.println(song); //Print Vote
+				
+				//loop to wait for other clients to vote
+				while(Lists.voteCast != Lists.clientNum)
+				{}
+				
+				
+				try
+				{
+					//If no song playing
+					if(Lists.firstcount)
 					{
-						song = Lists.songs[i];
+						int finalchoice = 0; //Most voted song
+						
+						//Find most popular choice
+						for(int i = 1; i<5; i++)
+						{
+							if(Lists.choice[i]>Lists.choice[finalchoice])
+								finalchoice=i;
+						}
+					
+						//Get File name of song
+						for(int i=0;i<Lists.tracks.length;i++)
+						{		
+							if(Lists.votes[finalchoice].equals(Lists.tracks[i]))
+							{
+								song = Lists.songs[i];
+							}
+						}
+						
+						//Play song
+						Process proc = Runtime.getRuntime().exec("C:\\Program Files (x86)\\foobar2000\\foobar2000.exe  C:\\Users\\Luke\\Desktop\\music\\"+song);
+						Lists.firstcount = false;
 					}
+					
+					//If song playing - queue next vote
+					else
+					{
+						int finalchoice = 0; //Most voted song
+						
+						//Find most popular choice
+						for(int i = 1; i<5; i++)
+						{
+							if(Lists.choice[i]>Lists.choice[finalchoice])
+								finalchoice=i;
+						}
+					
+						//Get File name of song
+						for(int i=0;i<Lists.tracks.length;i++)
+						{		
+							if(Lists.votes[finalchoice].equals(Lists.tracks[i]))
+							{
+								song = Lists.songs[i];
+							}
+						}
+						
+						//Add song to currently playing playlist
+						Process proc = Runtime.getRuntime().exec("C:\\Program Files (x86)\\foobar2000\\foobar2000.exe /add  C:\\Users\\Luke\\Desktop\\music\\"+song);
+					}
+					
+					//Wait
+					this.sleep(120000);
 				}
-
-				Process proc = Runtime.getRuntime().exec("C:\\Program Files (x86)\\foobar2000\\foobar2000.exe /add  C:\\Users\\Luke\\Desktop\\music\\"+song);
-			}
-
-			this.sleep(120000);
-		}
-		catch (Throwable e) 
-		{
-			System.out.println("Oh Lovely");
-		}
-			if (Lists.voteCast == Lists.clientNum)
+				catch (Throwable e) 
+				{
+					System.out.println("Oh Lovely");
+				}
+				if (Lists.voteCast == Lists.clientNum)
 				{
 					//New vote options
 					for (int i = 0; i < 5; i++)
 					{
 						Lists.votes[i] = Lists.tracks[RnG(18)];
 					}
-					Lists.voteCast = 0; //Reset
+					Lists.voteCast = 0; //Reset vote count
 				}
 
-		}
+			}
 		
 			
 		}
